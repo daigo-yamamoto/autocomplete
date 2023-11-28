dfbf8ddde9e4f9bc1aa3b7987fff8cd06665e905
Bruno Yamamoto <bruno.daigo.yamamoto@gmail.com>
2023-11-28 15:44:40 -0300

    implementacao do fst, falta so a funcao de minimizacao, ta funcionando igual o trie ainda

--- fst.py
+++ fst.py
@@ -1,44 +1,35 @@
 class FSTState:
-    def __init__(self):
+    def __init__(self, id):
+        self.id = id
         self.transitions = {}
-        self.output = None
+        self.outputs = {}
         self.is_final = False
 
 class FST:
     def __init__(self):
-        self.root = FSTState()
-        self.states = {self.root}
+        self.next_state_id = 0
+        self.root = self.create_state()
+        self.states = {self.root.id: self.root}
+
+    def create_state(self):
+        state = FSTState(self.next_state_id)
+        self.next_state_id += 1
+        return state
 
     def insert_word(self, word, output):
         current = self.root
         for char in word:
             if char not in current.transitions:
-                current.transitions[char] = FSTState()
-            current = current.transitions[char]
+                next_state = self.create_state()
+                self.states[next_state.id] = next_state
+                current.transitions[char] = next_state.id
+                current.outputs[char] = output
+            else:
+                current.outputs[char] = min(current.outputs[char], output)
+            current = self.states[current.transitions[char]]
         current.is_final = True
-        current.output = output
 
-    def find_minimized(self, state):
-        for s in self.states:
-            if s.transitions == state.transitions and s.output == state.output:
-                return s
-        self.states.add(state)
-        return state
-
-    def minimize_states(self, current, word, index):
-        if index < len(word):
-            char = word[index]
-            if char in current.transitions:
-                child_state = current.transitions[char]
-                minimized_state = self.find_minimized(child_state)
-                current.transitions[char] = minimized_state
-                self.minimize_states(minimized_state, word, index + 1)
-
-    def create_minimal_transducer(self, words, outputs):
-        previous_word = ""
-        for word, output in zip(words, outputs):
-            common_prefix_len = len(os.path.commonprefix([previous_word, word]))
-            self.minimize_states(self.root, previous_word, common_prefix_len)
-            self.insert_word(word, output)
-            previous_word = word
-        self.minimize_states(self.root, previous_word, 0)
+    def minimize(self):
+        # Placeholder for the minimize algorithm
+        # This should be replaced with the actual minimization code
+        pass
--- graph.py
+++ graph.py
@@ -17,33 +17,36 @@ def visualize_trie(trie):
     return graph
 
 def visualize_fst(fst):
-    def add_nodes_edges(node, node_label):
+    def add_nodes_edges(node_id, node_label, graph):
+        node = fst.states[node_id]  # Busca o objeto de estado real usando o ID
         if node.is_final:
             graph.node(node_label, shape='doublecircle')
-        for char, child_node in node.transitions.items():
+        for char, child_node_id in node.transitions.items():
             child_label = f"{node_label}{char}"
             graph.edge(node_label, child_label, label=char)
-            add_nodes_edges(child_node, child_label)
+            add_nodes_edges(child_node_id, child_label, graph)  # Passa o ID do filho
 
     graph = Digraph()
     graph.node("", shape='point')
-    add_nodes_edges(fst.root, "")
+    add_nodes_edges(fst.root.id, "", graph)  # Passa o ID do nó raiz
     return graph
 
-def load_words_from_file(file_path):
-    with open(file_path, 'r') as file:
-        return [line.strip() for line in file]
 
-# Carrega as palavras do arquivo
-file_path = './dicionario/teste.txt'  # Substitua pelo caminho correto do seu arquivo
-words = load_words_from_file(file_path)
+def load_words_and_outputs(file_path):
+    with open(file_path, 'r') as file:
+        words_outputs = [line.strip().split() for line in file]
+    return words_outputs
+
+# Caminho para o seu arquivo de texto
+file_path = './dicionario/teste.txt'
+words = load_words_and_outputs(file_path)
 
 # Instanciando e preenchendo a Trie e o FST
 trie = trie.Trie()
 fst = fst.FST()
-for word in words:
+for word, output in words:
     trie.insert_word(word)
-    fst.insert_word(word, "")
+    fst.insert_word(word, output)
 
 trie_graph = visualize_trie(trie)
 fst_graph = visualize_fst(fst)
